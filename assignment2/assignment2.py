a6474220d41ecad8335bf1ccad75288ed795f4d3
Lukas Jordbru <jordbru@Lukass-MacBook-Air.local>
2024-10-06 17:20:33 +0200

    commented code

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -18,6 +18,7 @@ plt.savefig("assignment2/Histogram of classes")
 
 # print(len(features))
 
+#Splitting the data 
 split = int(len(features)*0.8)
 
 training_features = features[:split]
@@ -26,6 +27,8 @@ training_classification = classification[:split]
 test_features = features[split:]
 test_classification = classification[split:]
 
+
+#Making the estmate fucntions
 def beta_estimater(x, alfa):
     first = 1/(len(x) * alfa)
     second = np.sum(x)
@@ -38,7 +41,7 @@ def sigma_sq_estimater(x, mu):
     sum = np.sum((x-mu)**2)
     return sum/len(x)
 
-
+#making the distribution functions
 def gamma_distribution(x, beta, alfa, gamma):
     exp = x**(alfa-1)*np.exp(-x/beta)
     div = beta**alfa * gamma
@@ -50,7 +53,7 @@ def gaussian_distribution(x, sigma, mu):
     return exp/div
 
 
-
+#Finding the estimates and probability for each class
 beta_est = beta_estimater(training_features, 2)
 mu_est = mu_estimater(training_features)
 sigma_est = np.sqrt(sigma_sq_estimater(training_features, mu_est))
@@ -58,11 +61,14 @@ sigma_est = np.sqrt(sigma_sq_estimater(training_features, mu_est))
 prob_class_C0 = gamma_distribution(test_features, beta_est, 2, 1)
 prob_class_C1 = gaussian_distribution(test_features, sigma_est, mu_est)
 
+#making a classifier functio
 def classifier(C0, C1):
     classific = np.zeros(len(C0))
     classific[C0<C1] = 1
     return classific
 
+
+#Findin the accuracy
 prob_classification = classifier(prob_class_C0, prob_class_C1)
 
 accuracy = np.mean(test_classification == prob_classification)*100
